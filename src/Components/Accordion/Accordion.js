cae9303c2070bc593efea329329f4eeda86d06d7
Ahmed Ramy <64094908+aahmedramy1@users.noreply.github.com>
2021-09-13 19:04:24 +0200

    feat: FAQ

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -3,26 +3,39 @@ import React, { useState } from "react";
 const FAQ = ({ question }) => {
   const [showBody, setShowBody] = useState(false);
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-between px-5 w-3/4 bg-blue-300 my-4 rounded-md h-11">
-        <header>{question.header}</header>
+    <div className="flex items-center justify-center flex-col my-4">
+      <div className="flex justify-center px-5 w-1/2 bg-blue-300 flex-col rounded-md h-auto">
+        <div className="flex items-center justify-between m-3">
+          <header>{question.header}</header>
+          {showBody ? (
+            <button
+              class="fas fa-times"
+              onClick={() => {
+                setShowBody((old) => !old);
+              }}
+            />
+          ) : (
+            <button
+              className="fas fa-plus"
+              onClick={() => {
+                setShowBody((old) => !old);
+              }}
+            />
+          )}
+        </div>
+
         {showBody ? (
-          <button
-            class="fas fa-times"
-            onClick={() => {
-              setShowBody((old) => !old);
-            }}
-          />
-        ) : (
-          <button
-            className="fas fa-plus"
-            onClick={() => {
-              setShowBody((old) => !old);
-            }}
-          />
-        )}
+          <div>
+            <div className="flex items-center justify-center mb-3">
+              <hr className="w-3/4" />
+            </div>
+
+            <div className=" mb-3 pl-3 pr-10 bg-blue-300">
+              <p>{question.body}</p>
+            </div>
+          </div>
+        ) : null}
       </div>
-      {showBody ? <p>{question.body}</p> : null}
     </div>
   );
 };
--- src/Containers/FAQ.js
+++ src/Containers/FAQ.js
@@ -5,6 +5,9 @@ import questions from "../Fixtures/FAQ.json";
 const FAQ = () => {
   return (
     <div>
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl">Frequently Asked Questions</h1>
+      </div>
       {questions.map((question) => {
         return <Accordion key={question.id} question={question} />;
       })}
