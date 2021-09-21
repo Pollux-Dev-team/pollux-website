b0c2dedf562d318436eca01016989ac3d7b81c09
Abdulrahman Hesham <62897733+Abdulrahmanrihan@users.noreply.github.com>
2021-09-21 02:09:18 +0200

    Added the header component to the App file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Contact from "./Components/Contact/Contact";
 import Form from "./Components/Form/Form";
 import ComponentPrices from "./Containers/ComponentPrices";
 import FAQ from "./Containers/FAQ";
+import Portfolio from "./Containers/portfolio";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       {/* <FAQ />
       <ComponentPrices />
-      <Contact /> */}
+      <Contact /> 
+      <Portfolio />
       <Form />
+      <Header />
+      */}
     </div>
   );
 }
