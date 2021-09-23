c6aa47ed66cd4eab573e77ce3b0d0a2476876265
Abdulrahman Rihan <bido4500@gmail.com>
2021-09-24 00:51:56 +0200

    style: :lipstick: Improved the responseveness and added the cake menu

--- src/Components/Mission/Mission.js
+++ src/Components/Mission/Mission.js
@@ -1,10 +1,10 @@
 function Mission() {
   return (
-    <div className="flex md:flex-row flex-col my-36 md:ml-0 ml-16 mr-16">
-      <div className="flex md:flex-row flex-col flex-1 md:-ml-12 md:justify-start ">
-        <div class="bg-blue-300 md:mx-0 m-auto my-auto rounded-full h-80 w-80 flex "></div>
+    <div className="flex md:flex-row flex-col mb-36 md:mt-36 mt-24 md:ml-0 ml-12 mr-12">
+      <div className="flex md:flex-row flex-col flex-1 md:-ml-12 mx-auto ">
+        <div class="bg-blue-300 md:mx-0 mx-auto my-auto rounded-full h-80 w-80 flex"></div>
         <div className="flex flex-col md:text-left text-center flex-1 my-auto z-10 md:-ml-48 -ml-0 md:mt-auto -mt-48 md:mb-auto -mb-0">
-          <h1 className="text-4xl font-bold my-2 ">Mission statement</h1>
+          <h1 className="md:text-4xl text-3xl font-bold my-2 ">Mission statement</h1>
           <div className="bg-yellow-500 w-32 h-1 my-2 md:mx-0 mx-auto"></div>
           <p className="my-2 font-bold text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
--- src/Components/Packages/Packages.js
+++ src/Components/Packages/Packages.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function Packages() {
   return (
-    <div className="flex flex-col mx-48 my-24">
+    <div className="flex flex-col md:mx-48 mx-12 my-24">
       <div className="flex justify-center items-center flex-col m-12">
         <h1 className="text-2xl font-bold mb-2">Our packages</h1>
         <div className="bg-yellow-500 w-16 h-1"></div>
       </div>
       <div className="flex md:flex-row flex-col m-5">
-        <div className="flex flex-col bg-blue-300 rounded-md m-5 p-8 md:w-80">
+        <div className="flex flex-col bg-blue-300 rounded-md m-5 p-8 md:flex-shrink-0 md:w-1/3">
           <div className="text-center font-bold text-xl p-1">Save money</div>
           <div className="text-center font-light text-white text-xl pb-1">
             choose pollux
@@ -25,15 +25,15 @@ function Packages() {
             make
           </p>
         </div>
-        <div className="flex flex-col mx-3 mt-8">
+        <div className="flex flex-col mx-3 mt-8 md:w-2/3">
           <div classname="flex flex-row text-left">
             <p className="text-2xl mx-3 font-bold italic">
               Start now with our basic package!
             </p>
           </div>
           <div className="flex md:flex-row flex-col my-5">
-            <div className="flex flex-col mx-3 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
-              <div className="font-bold text-center text-4xl p-6">19.99$</div>
+            <div className="flex md:w-1/3 flex-col mx-2 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
+              <div className="font-bold text-center lg:text-4xl text-2xl p-6">19.99$</div>
               <div className="text-center text-white">SILVER PACKAGE</div>
               <ul className="list-inside list-disc p-4">
                 <li className="py-2">Lorem ipsum is</li>
@@ -49,8 +49,8 @@ function Packages() {
                 <button>Purchase</button>
               </Link>
             </div>
-            <div className="flex flex-col mx-3 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
-              <div className="font-bold text-center text-4xl p-6">19.99$</div>
+            <div className="flex md:w-1/3 flex-col mx-2 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
+              <div className="font-bold text-center lg:text-4xl text-2xl p-6">19.99$</div>
               <div className="text-center text-white">SILVER PACKAGE</div>
               <ul className="list-inside list-disc p-4">
                 <li className="py-2">Lorem ipsum is</li>
@@ -66,8 +66,8 @@ function Packages() {
                 <button>Purchase</button>
               </Link>
             </div>
-            <div className="flex flex-col mx-3 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
-              <div className="font-bold text-center text-4xl p-6">19.99$</div>
+            <div className="flex md:w-1/3 flex-col mx-2 md:my-0 my-3 bg-blue-300 p-3 rounded-md">
+              <div className="font-bold text-center lg:text-4xl text-2xl p-6">19.99$</div>
               <div className="text-center text-white">SILVER PACKAGE</div>
               <ul className="list-inside list-disc p-4">
                 <li className="py-2">Lorem ipsum is</li>
@@ -85,7 +85,6 @@ function Packages() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col"></div>
       </div>
     </div>
   );
